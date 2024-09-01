66e88d5c061edae0dc7e2c71c9335ace60155faf
= <=>
2024-09-01 19:24:22 +0500

    commit

--- app/admin/Sidebar.tsx
+++ app/admin/Sidebar.tsx
@@ -15,12 +15,11 @@ function Sidebar() {
         return (
           <Button
             asChild
-            className="w-full mb-2 capitalize font-normal justify-start"
+            className='w-full mb-2 capitalize font-normal justify-start'
             variant={variant}
+            key={link.href}
           >
-            <Link key={link.href} href={link.href}>
-              {link.label}
-            </Link>
+            <Link href={link.href}>{link.label}</Link>
           </Button>
         );
       })}
