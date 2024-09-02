574cc3aed271d7d0cacf4fa6aa1e2bd0979aca06
= <=>
2024-09-02 12:31:21 +0500

    commit

--- app/admin/Sidebar.tsx
+++ app/admin/Sidebar.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import { adminLinks } from "@/utils/links";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
@@ -7,23 +7,6 @@ import { Button } from "@/components/ui/button";
 function Sidebar() {
   const pathname = usePathname();
 
-  return (
-    <aside>
-      {adminLinks.map((link) => {
-        const isActivePage = pathname === link.href;
-        const variant = isActivePage ? "default" : "ghost";
-        return (
-          <Button
-            asChild
-            className='w-full mb-2 capitalize font-normal justify-start'
-            variant={variant}
-            
-          >
-            <Link key={link.href} href={link.href}>{link.label}</Link>
-          </Button>
-        );
-      })}
-    </aside>
-  );
+  return <aside>Admin</aside>;
 }
 export default Sidebar;
