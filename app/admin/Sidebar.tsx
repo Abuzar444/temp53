d1004bad7e2a5f2aff20f905ea53610e74a9bc8c
= <=>
2024-09-02 12:34:41 +0500

    commit

--- app/admin/Sidebar.tsx
+++ app/admin/Sidebar.tsx
@@ -1,12 +1,4 @@
-"use client";
-import { adminLinks } from "@/utils/links";
-import Link from "next/link";
-import { usePathname } from "next/navigation";
-import { Button } from "@/components/ui/button";
-
-function Sidebar() {
-  const pathname = usePathname();
-
-  return <aside>Admin</aside>;
-}
+const Sidebar = () => {
+  return <div>Sidebar</div>;
+};
 export default Sidebar;
